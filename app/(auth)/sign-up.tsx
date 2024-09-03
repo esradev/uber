f057b591c11bc2b5c9e46060f716732fcf478f8b
Wpstorm <wpstormdev@gmail.com>
2024-09-04 00:38:13 +0330

    First Commit!

---
+++ app/(auth)/sign-up.tsx
@@ -0,0 +1,72 @@
+import { Text, ScrollView, View, Image } from "react-native";
+import { icons, images } from "@/constants";
+import InputField from "@/components/InputField";
+import { useState } from "react";
+import CustomButton from "@/components/CustomButton";
+import { Link } from "expo-router";
+
+const onSignUpPress = () => {};
+const SignUp = () => {
+  const [form, setForm] = useState({
+    name: "",
+    email: "",
+    password: "",
+  });
+
+  return (
+    <ScrollView className="flex-1 bg-white">
+      <View className="flex bg-white">
+        <View className="relative w-full h-[250px]">
+          <Image source={images.signUpCar} className="z-0 w-full h-[250px]" />
+          <Text className="text-2xl text-black font-JakartaSemiBold absolute bottom-5 left-5">
+            Create Your Account
+          </Text>
+        </View>
+
+        <View className="p-5">
+          <InputField
+            label="Name"
+            placeholder="Enter your name"
+            icon={icons.person}
+            value={form.name}
+            onChange={(value) => setForm({ ...form, name: value })}
+          />
+          <InputField
+            label="Email"
+            placeholder="Enter your email"
+            icon={icons.email}
+            value={form.email}
+            onChange={(value) => setForm({ ...form, email: value })}
+          />
+          <InputField
+            label="Password"
+            placeholder="Enter your password"
+            icon={icons.lock}
+            value={form.password}
+            onChange={(value) => setForm({ ...form, password: value })}
+            secureTextEntry={true}
+          />
+
+          <CustomButton
+            title="Sign Up"
+            onPress={onSignUpPress}
+            className="mt-6"
+          ></CustomButton>
+
+          {/*  OAuth */}
+          <Link
+            href="/sign-in"
+            className="text-lg text-general-200 text-center mt-10"
+          >
+            <Text>Already have an account? </Text>
+            <Text className="text-primary-500">Log In</Text>
+          </Link>
+        </View>
+
+        {/*  Verification modal*/}
+      </View>
+    </ScrollView>
+  );
+};
+
+export default SignUp;
